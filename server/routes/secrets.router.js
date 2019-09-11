a06fbd414a2801c83ac2f554c094a345cafb1bf3
Luke Becker <beckerluke1@gmail.com>
2019-09-11 14:55:13 -0500

    Modified secrets router GET request to ensure that unauthenticated users are not allowed access to secrets

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -2,14 +2,34 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// router.get('/', (req, res) => {
+//     console.log('req.user:', req.user);
+//     pool.query('SELECT * FROM "secret";')
+//         .then(results => res.send(results.rows))
+//         .catch(error => {
+//             console.log('Error making SELECT for secrets:', error);
+//             res.sendStatus(500);
+//         });
+// });
+
 router.get('/', (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
-        .then(results => res.send(results.rows))
-        .catch(error => {
-            console.log('Error making SELECT for secrets:', error);
-            res.sendStatus(500);
-        });
+    const queryText = `SELECT * FROM "user" WHERE "clearance_level" <= $1;`;
+    // see if user is authenticated and if so respond with data user is cleared for
+    // if user is not authenticated respond with forbidden error
+    if (req.isAuthenticated()) {
+        pool.query(queryText,[req.user.clearance_level])
+            .then((response) => {
+                res.send(response.rows);
+            })
+            .catch((err) => {
+                console.log(`${err}`);
+                res.sendStatus(500);
+            })   
+    } else {
+        res.sendStatus(403);
+    }
 });
+
 
 module.exports = router;
